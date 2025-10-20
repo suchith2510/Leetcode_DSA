4c76bf54daf6d5efee3b4e5f87bfc4abef0f2d13
suchThat <suchithpuppala@gmail.com>
2025-10-20 08:04:54 +0530

    Time: 5 ms (54.61%), Space: 42.9 MB (89.38%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,19 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int[] arr1=new int[26];
+        int[] arr2=new int[26];
+        if(s.length()==t.length()){
+        for(int i=0;i<s.length();i++){
+            arr1[s.charAt(i)-'a']++;
+            arr2[t.charAt(i)-'a']++;
+        }
+        for(int i=0;i<26;i++){
+            if(arr1[i]!=arr2[i]){
+                return false;
+            }
+        }
+        return true;
+        }
+        return false;
+    }
+}
