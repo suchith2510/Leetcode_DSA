6cf6ffdd6ba177700ddf48ec3d1842704c1f1473
suchThat <suchithpuppala@gmail.com>
2026-04-28 14:08:44 +0530

    Time: 2 ms (70.93%), Space: 55.4 MB (67.05%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -1,16 +1,11 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer,Integer> hs=new HashMap<>();
-        int max=0;
-        int res=0;
-        for(int num: nums){
-            int count=hs.getOrDefault(num,0)+1;
-            hs.put(num,count);
-            if(count>max){
-                max=count;
-                res=num;
-            }
+        int count=0, candidate=0;
+        for(int i:nums){
+            if(count==0) candidate=i;
+            if(i==candidate) count++;
+            else count--;
         }
-        return res;
+        return candidate;
     }
 }
