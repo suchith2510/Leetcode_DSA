9f69ec72f6f7ac01810873e34c04528556221fea
suchThat <suchithpuppala@gmail.com>
2026-04-28 12:15:56 +0530

    Time: 0 ms (100%), Space: 43.6 MB (25.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,5 +1,24 @@
 class Solution {
     public void sortColors(int[] nums) {
-        Arrays.sort(nums);
+        int i=0,l=0,r=nums.length-1;
+        while(i<=r){
+            if(nums[i]==0){
+                swap(nums,l,i);
+                i++;
+                l++;
+            }
+            else if(nums[i]==1){
+                i++;
+            }
+            else{
+                swap(nums,r,i);
+                r--;
+            }
+            }
+        }
+    public void swap(int[] nums, int x, int y){
+        int temp=nums[x];
+        nums[x]=nums[y];
+        nums[y]=temp;
     }
 }
