5ee5e08d9e93bc0b6e93f3ba52bb1777229a00dd
suchThat <suchithpuppala@gmail.com>
2026-09-18 10:55:55 +0530

    Time: 19 ms (94.63%), Space: 253.4 MB (93.75%) - LeetHub

---
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -0,0 +1,27 @@
+class Solution {
+    int leader[];
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        leader =new int[n];
+        for(int i=0;i<n;i++){
+            leader[i]=i;
+        }
+            for(int[] edge:edges){
+                int u=edge[0],v=edge[1];
+                merge(u,v);
+            }
+        int slu=find(source);
+        int slv=find(destination);
+        return slu==slv;
+       
+    }
+    public void merge(int u, int v){
+        int slu=find(u);
+        int slv=find(v);
+        leader[slv]=slu;
+    }
+    public int find(int x){
+        if(leader[x]==x) return x;
+        leader[x]= find(leader[x]);
+        return leader[x];
+    }
+}
